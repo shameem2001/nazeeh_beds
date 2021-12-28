819b77e72557d2cf7bf07ac9e88d9f25be63f9b1
shameem2001 <toshameem2001@gmail.com>
2021-12-28 09:48:02 +0530

    Added WillpopScope

--- lib/screens/product_list_page.dart
+++ lib/screens/product_list_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'homepage.dart';
+
 class ProductListPage extends StatefulWidget {
   static const String id = "product_list_page";
   const ProductListPage({Key key}) : super(key: key);
@@ -11,10 +13,21 @@ class ProductListPage extends StatefulWidget {
 class _ProductListPageState extends State<ProductListPage> {
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        appBar: AppBar(),
-        body: Container(),
+    return WillPopScope(
+      onWillPop: () async {
+        if (Navigator.canPop(context)) {
+          Navigator.pop(context);
+          return true;
+        } else {
+          Navigator.popAndPushNamed(context, HomePage.id);
+          return false;
+        }
+      },
+      child: SafeArea(
+        child: Scaffold(
+          appBar: AppBar(),
+          body: Container(),
+        ),
       ),
     );
   }
